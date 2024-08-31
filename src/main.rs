8fe95b7fe5cc3c4115632330cd68239ab30b2fb7
gsfsnsiciesa <2083886+GSFSNSICIESA@users.noreply.github.com>
2024-08-31 12:27:25 +0300

    solved issue #8

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,28 @@ struct OOPS {
     current_file: PathBuf,
     tmp_buffer: String,
     current_file_is_saved: bool,
-    current_path: PathBuf,
+    current_directory: PathBuf,
 }
+
 impl OOPS {
     fn new() -> Self {
         Self {
             current_file_is_saved: true,
             ..Default::default()
+        }
+    }
+    fn save_file(&mut self, path: String) {
+        if path == self.current_file.to_str().unwrap() {
+            if fs::write(self.current_file.clone(), self.buffer.clone()).is_ok() {
+                self.tmp_buffer = self.buffer.clone();
+                self.check_if_changed();
+            };
+        } else {
+            self.current_file = PathBuf::from(path);
+            if fs::write(self.current_file.clone(), self.buffer.clone()).is_ok() {
+                self.tmp_buffer = self.buffer.clone();
+                self.check_if_changed();
+            };
         }
     }
 }
@@ -41,7 +56,7 @@ impl eframe::App for OOPS {
                             Some(current_file) => {
                                 let mut tmp_path = current_file.clone();
                                 tmp_path.pop();
-                                self.current_path = tmp_path;
+                                self.current_directory = tmp_path;
                                 self.current_file = current_file;
                             }
                             None => {}
@@ -73,10 +88,17 @@ impl eframe::App for OOPS {
 
         egui::TopBottomPanel::bottom("status bar").show(ctx, |ui| {
             if ui.button("Save File").clicked() {
-                if fs::write(self.current_file.clone(), self.buffer.clone()).is_ok() {
-                    self.tmp_buffer = self.buffer.clone();
-                    self.check_if_changed();
-                };
+                if self.current_file.exists() {
+                    self.save_file(self.current_file.to_str().unwrap().to_string());
+                } else {
+                    if let Some(file) = FileDialog::new().set_directory("/").save_file() {
+                        let mut tmp_path = file.clone();
+                        tmp_path.pop();
+                        self.current_directory = tmp_path;
+                        self.current_file = file;
+                        self.save_file(self.current_file.to_str().unwrap().to_string());
+                    }
+                }
             }
             if ui.button("check if the file is saved").clicked() {
                 self.check_if_changed();
@@ -84,15 +106,19 @@ impl eframe::App for OOPS {
         });
         egui::SidePanel::left("file navigation").show(ctx, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
-                if let Ok(files) = fs::read_dir(self.current_path.clone()) {
-                    let mut sorted_files: Vec<OsString> =
-                        files.map(|file| file.unwrap().file_name()).collect();
+                if let Ok(files) = fs::read_dir(self.current_directory.clone()) {
+                    let mut sorted_files: Vec<OsString> = files
+                        .map(|file| file.unwrap().path().as_os_str().to_os_string())
+                        .collect();
                     sorted_files.sort();
                     for file in sorted_files {
+                        let tmp_path = PathBuf::from(file);
+
                         let label =
-                            egui::Label::new(file.to_str().unwrap()).sense(egui::Sense::click());
+                            egui::Label::new(tmp_path.file_name().unwrap().to_str().unwrap())
+                                .sense(egui::Sense::click());
                         if ui.add(label).clicked() {
-                            self.current_file = PathBuf::from(file);
+                            self.current_file = tmp_path;
                             let contents = fs::read_to_string(self.current_file.clone());
                             match contents {
                                 Ok(buffer) => {
